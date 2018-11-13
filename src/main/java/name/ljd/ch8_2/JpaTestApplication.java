ee850bf13aba022a7a5054c904fd4732215d5277
liujidong <wwwliujidong@163.com>
2018-11-13 09:50:56 +0800

    init

---
+++ src/main/java/name/ljd/ch8_2/JpaTestApplication.java
@@ -0,0 +1,12 @@
+package name.ljd.ch8_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTestApplication.class, args);
+	}
+}
