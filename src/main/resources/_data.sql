6eb9afbe872c666c4d5ee259e211a9eeb6a92444
liujidong <wwwliujidong@163.com>
2018-11-13 18:05:39 +0800

    数据库切换 oracle->derby

--- src/main/resources/_data.sql
+++ src/main/resources/_data.sql
@@ -1,6 +1,9 @@
-insert into person(id,name,age,address) values(hibernate_sequence.nextval,'汪云飞',32,'合肥');
-insert into person(id,name,age,address) values(hibernate_sequence.nextval,'xx',31,'北京');
-insert into person(id,name,age,address) values(hibernate_sequence.nextval,'yy',30,'上海');
-insert into person(id,name,age,address) values(hibernate_sequence.nextval,'zz',29,'南京');
-insert into person(id,name,age,address) values(hibernate_sequence.nextval,'aa',28,'武汉');
-insert into person(id,name,age,address) values(hibernate_sequence.nextval,'bb',27,'合肥');	
+--ij
+-- connect 'jdbc:derby://localhost:1527/db-test;create=true;user=boot';
+--create sequence hibernate_sequence as int;
+insert into person(id,name,age,address) values(next value for hibernate_sequence,'汪云飞',32,'合肥');
+insert into person(id,name,age,address) values(next value for hibernate_sequence,'xx',31,'北京');
+insert into person(id,name,age,address) values(next value for hibernate_sequence,'yy',30,'上海');
+insert into person(id,name,age,address) values(next value for hibernate_sequence,'zz',29,'南京');
+insert into person(id,name,age,address) values(next value for hibernate_sequence,'aa',28,'武汉');
+insert into person(id,name,age,address) values(next value for hibernate_sequence,'bb',27,'合肥');	
