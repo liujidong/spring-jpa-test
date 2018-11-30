0c640b674af25dc3ad9b7978b6245e573e9399ea
liujidong <wwwliujidong@163.com>
2018-11-30 10:35:40 +0800

    1st commit

---
+++ src/main/java/name/ljd/ch8_6/JpaNoSqlApplication.java
@@ -0,0 +1,12 @@
+package name.ljd.ch8_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaNoSqlApplication.class, args);
+	}
+}
